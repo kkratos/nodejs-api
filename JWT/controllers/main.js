8662be00a18caabec521f5120f11ded400c1ca26
kamal patel <kamal.patel30@gmail.com>
2022-06-26 15:28:06 +0530

    jwt authorization

--- JWT/controllers/main.js
+++ JWT/controllers/main.js
@@ -26,11 +26,25 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100);
-  res.status(200).json({
-    msg: `Hello, John Doe`,
-    secret: `Here is your authorized data, you lucky number is ${luckyNumber}`,
-  });
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided ", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is your authorized data, you lucky number is ${luckyNumber}`,
+    });
+  } catch (err) {
+    throw new CustomAPIError("No authorized to access this route", 401);
+  }
 };
 
 module.exports = {
